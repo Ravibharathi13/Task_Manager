561a87b370f42105dc8949621b6330a4b0e0a287
ravibharathi13 <ravibharathimoorthy2005@gmail.com>
2025-12-12 10:23:05 +0530

    latest

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'https://task-manager-ccrl.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || "https://task-manager-ccrl.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
